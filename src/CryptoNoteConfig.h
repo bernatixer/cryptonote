8ed91df19f965678f774d6806db7bf1153aa0ab8
Bernat Torres <bernat.torres.bellido@gmail.com>
2018-01-18 19:49:19 +0100

    Fixing build error & Adding genesis block

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -64,7 +64,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 } // parameters
 
 const char     CRYPTONOTE_NAME[]                             = "tixercoin";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff0001f9c484c69cd608029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807121012ab9dc298adaf713921d3a76a437c297ac3430f62433f9cd187196b2d883fb2a";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
